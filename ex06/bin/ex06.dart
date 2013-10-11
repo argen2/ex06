4ade18e806c32c615416ea2ca38d69b3be3ed37f
argen2 <chiefmulot@gmail.com>
2013-10-11 16:49:35 -0400

    titre des exercices

--- ex06/bin/ex06.dart
+++ ex06/bin/ex06.dart
@@ -1,3 +1,37 @@
-void main() {
-  print("Hello, World!");
+library ex06.dart;
+
+part 'arguments.dart';
+
+void main(){
+
+  print('Exercice 6.1 - Verification si un texte est un Palindrome');
+  print('');
+  print('''Definition du palindrome : Figure de style designant un mot dont l'ordre
+des lettres reste le meme qu on le lise de gauche a droite ou de droite a gauche''');
+print('');
+
+
+
+
+print('Exercice 6.2 - Calculer le nombre de jour entre deux dates');
+print('');
+
+
+
+
+print('Exercice 6.3 - Conversion des notes en chiffres en notes en lettre');
+print('');
+
+
+
+
+print('Exercice 6.4 - Avec une liste de nom, faire 3 listes - < que 8 - > que 8 c - = 8');
+print('');
+
+
+
+
+print('Exercice 6.5 - Classer des joueurs par ordre d equipe');
+print('');
+    
 }
