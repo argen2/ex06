b610c5c80dbdb432984edd417e04e91c041dae19
argen2 <chiefmulot@gmail.com>
2013-10-12 09:05:50 -0400

    ajout de lexercice 6.1

--- ex06/bin/arguments.dart
+++ ex06/bin/arguments.dart
@@ -1,2 +1,17 @@
 part of ex06;
 
+bool Palindrome(String text) {
+  
+  int debut = 0;
+  int fin = text.length - 1;
+  
+  while (debut < fin) {
+    if (text[debut] != text[fin]) {
+      return false;
+    }
+    debut = debut + 1;
+    fin = fin - 1;
+  }
+  
+  return true;
+}
--- ex06/bin/ex06.dart
+++ ex06/bin/ex06.dart
@@ -2,13 +2,37 @@ library ex06.dart;
 
 part 'arguments.dart';
 
-void main(){
+void ex06_1(){
 
   print('Exercice 6.1 - Verification si un texte est un Palindrome');
   print('');
   print('''Definition du palindrome : Figure de style designant un mot dont l'ordre
 des lettres reste le meme qu on le lise de gauche a droite ou de droite a gauche''');
 print('');
+var texte1 = "elleatropparrapportaelle";
+var answer1='';
+print('Premier texte : Elle a trop rapport a elle');
+answer1 = Palindrome(texte1);
+if (answer1==true) {
+  print('Reponse : Vrai - Le texte est un palindrome');}
+else {print('Reponse : Faux - Le texte n est pas un palindrome');}
+print('');
+print('Second texte : Elu par cette crapule');
+var texte2 = "eluparcettecrapule";
+var answer2='';
+answer2 = Palindrome(texte2);
+if (answer1==true) {
+  print('Reponse : Vrai - Le texte est un palindrome');}
+else {print('Reponse : Faux - Le texte n est pas un palindrome');}
+print('');
+print('Troisieme texte : Le canadien va gagne la coupe');
+var texte3 = "lecanadienvagagnelacoupe";
+var answer3='';
+answer2 = Palindrome(texte3);
+if (answer3==true) {
+  print('Reponse : Vrai - Le texte est un palindrome');}
+else {print('Reponse : Faux - Le texte n est pas un palindrome');}
+print('-------------------------------------');
 
 
 
@@ -34,4 +58,7 @@ print('');
 print('Exercice 6.5 - Classer des joueurs par ordre d equipe');
 print('');
     
+}
+void main(){
+  ex06_1();
 }
